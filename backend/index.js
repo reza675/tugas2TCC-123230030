6adb89ef58660c7ef4acdbb6574f8db48dfb6e08
Reza Rasendriya <reza58912@gmail.com>
2026-03-19 15:46:44 +0700

    erm

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ const app = express();
 const cors = require("cors");
 
 app.use(cors({
-  origin: ['http://localhost', 'http://localhost:5173', 'http://127.0.0.1:5500'],
+  origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
 }));
